d916788b2d519829fe463a1df1fdf88d5b2992de
Yaroslav Guz <aroslavguz@gmail.com>
2025-02-05 21:13:35 +0200

    Fix some problems

--- cinema_frontend/src/App.jsx
+++ cinema_frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import InCinemaPage from './pages/InCinemaPage';
+import InCinemaPage from './pages/InCinemaPage/InCinemaPage';
 
 const App = () => {
   return (
