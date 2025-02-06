babba62282da6ff9050ffdcabef769700ba2a807
Yaroslav Guz <aroslavguz@gmail.com>
2025-02-06 11:13:58 +0200

    Create GoToHomePage component

--- cinema_frontend/src/pages/AuthPage/AuthPage.jsx
+++ cinema_frontend/src/pages/AuthPage/AuthPage.jsx
@@ -1,9 +1,14 @@
+import GoToHomePage from '@/components/GoToHomePage/GoToHomePage';
 import RegisterLoginCard from '@/components/RegisterLoginCard/RegisterLoginCard';
 import React from 'react';
 
 const AuthPage = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+    <div className="relative min-h-screen bg-gray-100 flex justify-center items-center">
+      <div className="absolute top-4 left-4">
+        <GoToHomePage />
+      </div>
+
       <RegisterLoginCard />
     </div>
   );
