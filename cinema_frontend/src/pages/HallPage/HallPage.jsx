5dc20aab9212c056f1a40fa3a657175e2bc54ff7
Yaroslav Guz <aroslavguz@gmail.com>
2025-02-07 11:00:52 +0200

    Add GoToHomePage component

--- cinema_frontend/src/pages/HallPage/HallPage.jsx
+++ cinema_frontend/src/pages/HallPage/HallPage.jsx
@@ -1,8 +1,13 @@
+import GoToHomePage from '@/components/GoToHomePage/GoToHomePage';
 import React from 'react';
 
 const HallPage = () => {
   return (
-    <div>HallPage</div>
+    <div className="relative min-h-screen bg-gray-100 flex">
+      <div className="absolute top-4 left-4">
+        <GoToHomePage message={"Choose tickets"} />
+      </div>
+    </div>
   )
 }
 
