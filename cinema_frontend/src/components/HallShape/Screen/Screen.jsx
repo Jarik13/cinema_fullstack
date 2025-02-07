e18d17e9a4c26f46a76be404821f9289d828d3be
Yaroslav Guz <aroslavguz@gmail.com>
2025-02-07 11:32:01 +0200

    Create screen component

--- cinema_frontend/src/components/HallShape/Screen/Screen.jsx
+++ cinema_frontend/src/components/HallShape/Screen/Screen.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
 const Screen = () => {
-  return (
-    <div>Screen</div>
-  )
-}
+    return (
+        <div className="relative text-center text-xl w-full">
+            <div className="absolute inset-x-0 top-0 h-24 bg-gradient-to-b from-gray-400 via-transparent to-transparent rounded-t-xl shadow-[0px_-15px_50px_rgba(255,255,255,0.2)]"></div>
+
+            <div className="relative w-3/4 mx-auto">
+                <div
+                    className="h-6 w-full bg-gradient-to-r from-gray-600 via-gray-700 to-gray-600 shadow-lg p-4"
+                    style={{
+                        borderRadius: '0 0 100px 100px',
+                    }}
+                >
+                    <h2 className="text-white absolute inset-0 flex items-center justify-center">Screen</h2>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default Screen;
