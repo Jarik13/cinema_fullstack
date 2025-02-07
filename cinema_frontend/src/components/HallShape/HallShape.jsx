56308e0f29339033217126cd1a66f104112d5e7a
Yaroslav Guz <aroslavguz@gmail.com>
2025-02-07 13:06:58 +0200

    Fix error

--- cinema_frontend/src/components/HallShape/HallShape.jsx
+++ cinema_frontend/src/components/HallShape/HallShape.jsx
@@ -4,7 +4,7 @@ import Screen from './Screen/Screen';
 
 const HallShape = () => {
   return (
-    <div className='flex flex-col w-3/4 gap-10'>
+    <div className='flex flex-col w-3/4 gap-10 bg-gray-100 p-2 rounded-lg'>
         <Screen />
         <SeatsList />
     </div>
--- cinema_frontend/src/components/HallShape/Screen/Screen.jsx
+++ cinema_frontend/src/components/HallShape/Screen/Screen.jsx
@@ -3,11 +3,9 @@ import React from 'react';
 const Screen = () => {
     return (
         <div className="relative text-center text-xl w-full">
-            <div className="absolute inset-x-0 top-0 h-8 bg-gradient-to-b from-gray-400 via-transparent to-transparent rounded-t-xl shadow-[0px_-15px_50px_rgba(255,255,255,0.2)]"></div>
-
             <div className="relative w-3/4 mx-auto">
                 <div
-                    className="h-6 w-full bg-gradient-to-r from-gray-600 via-gray-700 to-gray-600 shadow-lg p-4"
+                    className="h-6 w-full bg-gradient-to-r from-gray-600 via-gray-700 to-gray-600 p-4"
                     style={{
                         borderRadius: '0 0 100px 100px',
                     }}
