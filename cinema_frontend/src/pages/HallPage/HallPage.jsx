1d8c1cc1485be4bc6c6b8b71625a9bc98d1f5ca0
Yaroslav Guz <aroslavguz@gmail.com>
2025-02-07 11:10:36 +0200

    Add session data

--- cinema_frontend/src/pages/HallPage/HallPage.jsx
+++ cinema_frontend/src/pages/HallPage/HallPage.jsx
@@ -3,9 +3,26 @@ import React from 'react';
 
 const HallPage = () => {
   return (
-    <div className="relative min-h-screen bg-gray-100 flex">
-      <div className="absolute top-4 left-4">
-        <GoToHomePage message={"Choose tickets"} />
+    <div className='flex flex-col w-full p-4'>
+      <GoToHomePage message={"Issuing tickets"} />
+
+      <div className='flex flex-col justify-start p-2 mt-4 border-b-2'>
+        <div className='text-2xl mb-2'>
+          <h1>Choose seats in Hall</h1>
+        </div>
+        <div className='flex gap-4 items-center'>
+          <span>Film Name</span>
+          <span>•</span>
+          <span>7 february</span>
+          <span>•</span>
+          <div className='flex gap-2'>
+            <span>start_time</span>
+            <span>-</span>
+            <span>end_time</span>
+          </div>
+          <span>•</span>
+          <span>Hall Name</span>
+        </div>
       </div>
     </div>
   )
