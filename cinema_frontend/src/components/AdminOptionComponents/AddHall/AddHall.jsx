ce4183ed3f8449372bf6c5a0d83757ef25a8e450
Yaroslav Guz <aroslavguz@gmail.com>
2025-02-08 15:55:37 +0200

    Add hall name field

--- cinema_frontend/src/components/AdminOptionComponents/AddHall/AddHall.jsx
+++ cinema_frontend/src/components/AdminOptionComponents/AddHall/AddHall.jsx
@@ -1,9 +1,52 @@
-import React from 'react';
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
 
 const AddHall = () => {
-  return (
-    <div>AddHall</div>
-  )
-}
+    const form = useForm({
+        defaultValues: {
+            name: "",
+        },
+        mode: "onChange",
+    });
+
+    const { handleSubmit } = form;
+
+    const onSubmit = (data) => {
+        console.log(data);
+    }
+
+    return (
+        <div className="p-4 border rounded bg-gray-50 w-1/2">
+            <h2 className="text-xl font-bold mb-4">Add a New Hall</h2>
+            <Form {...form}>
+                <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+                    <FormField
+                        control={form.control}
+                        name="name"
+                        rules={{ required: "Name is required" }}
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Name</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="Hall name ..." {...field} />
+                                </FormControl>
+                                <FormDescription>
+                                    This is your public name of hall.
+                                </FormDescription>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+                    <Button type="submit" variant="destructive">
+                        Save All
+                    </Button>
+                </form>
+            </Form>
+        </div>
+    );
+};
 
 export default AddHall;
