493778a76b591d601696fc253caacc285cd96e0a
Yaroslav Guz <aroslavguz@gmail.com>
2025-02-08 12:02:46 +0200

    Add navigation to list of films page

--- cinema_frontend/src/pages/FilmDetailsOnlinePage/FilmDetailsOnlinePage.jsx
+++ cinema_frontend/src/pages/FilmDetailsOnlinePage/FilmDetailsOnlinePage.jsx
@@ -1,9 +1,12 @@
+import GoToHomePage from '@/components/GoToHomePage/GoToHomePage';
 import React from 'react';
 
 const FilmDetailsOnlinePage = () => {
-  return (
-    <div>FilmDetailsOnlinePage</div>
-  )
+    return (
+        <div className='flex flex-col w-full p-4'>
+            <GoToHomePage message={"Watch film online"} navigation={"/watch-online"} />
+        </div>
+    )
 }
 
 export default FilmDetailsOnlinePage;
