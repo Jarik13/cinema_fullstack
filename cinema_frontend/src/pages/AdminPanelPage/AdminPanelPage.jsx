e7d5eb2714024fa1bc47328f79f5e07a9030fedc
Yaroslav Guz <aroslavguz@gmail.com>
2025-02-08 15:25:39 +0200

    Create admin options for the hall

--- cinema_frontend/src/pages/AdminPanelPage/AdminPanelPage.jsx
+++ cinema_frontend/src/pages/AdminPanelPage/AdminPanelPage.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import { Button } from "@/components/ui/button";
+import React, { useState } from "react";
 
 const AdminPanelPage = () => {
-  return (
-    <div>AdminPanelPage</div>
-  )
-}
+    return (
+        <div className="flex">
+            <div className="w-1/4 bg-gray-100 h-screen p-4">
+                <div className="space-y-4">
+                    <Button variant="destructive" className="w-full py-2 px-4">
+                        Add halls
+                    </Button>
+                    <Button variant="outline" className="w-full py-2 px-4">
+                        List halls
+                    </Button>
+                </div>
+            </div> 
+        </div>
+    );
+};
 
 export default AdminPanelPage;
