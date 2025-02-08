cd5af95f2e48fb90fc80c3361bf2dacf851cbfac
Yaroslav Guz <aroslavguz@gmail.com>
2025-02-08 17:05:29 +0200

    Create DeleteHallCard modal component

--- cinema_frontend/src/components/AdminOptionComponents/ListOfHalls/DeleteHallCard/DeleteHallCard.jsx
+++ cinema_frontend/src/components/AdminOptionComponents/ListOfHalls/DeleteHallCard/DeleteHallCard.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import { Button } from '@/components/ui/button';
+import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 
-const DeleteHallCard = () => {
-  return (
-    <div>DeleteHallCard</div>
-  )
-}
+const DeleteHallModal = ({ isOpen, onClose, onConfirm, hallName }) => {
+    return (
+        <Dialog open={isOpen} onOpenChange={onClose}>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Confirm Deletion</DialogTitle>
+                </DialogHeader>
+                <p>Are you sure you want to delete the hall "{hallName}"?</p>
+                <DialogFooter>
+                    <Button variant="outline" onClick={onClose}>Cancel</Button>
+                    <Button variant="destructive" onClick={onConfirm}>Delete</Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    );
+};
 
-export default DeleteHallCard;
+export default DeleteHallModal;
