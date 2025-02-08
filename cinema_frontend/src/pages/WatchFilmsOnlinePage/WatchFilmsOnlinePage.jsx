5dd5f27f8bb6e1b75dc5b33f515db63d362fdc6d
Yaroslav Guz <aroslavguz@gmail.com>
2025-02-08 11:24:15 +0200

    Add navigation to home page

--- cinema_frontend/src/pages/WatchFilmsOnlinePage/WatchFilmsOnlinePage.jsx
+++ cinema_frontend/src/pages/WatchFilmsOnlinePage/WatchFilmsOnlinePage.jsx
@@ -1,9 +1,12 @@
+import GoToHomePage from '@/components/GoToHomePage/GoToHomePage';
 import React from 'react';
 
 const WatchFilmsOnlinePage = () => {
-  return (
-    <div>WatchFilmsOnlinePage</div>
-  )
+    return (
+        <div className='flex flex-col w-full p-4'>
+            <GoToHomePage message={"Watch films online"} navigation={"/"} />
+        </div>
+    );
 }
 
 export default WatchFilmsOnlinePage;
