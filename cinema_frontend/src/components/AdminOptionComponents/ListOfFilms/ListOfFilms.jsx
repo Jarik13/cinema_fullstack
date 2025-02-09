59cb4f50cb57b534054e9ab43b6b8db999f352a2
Yaroslav Guz <aroslavguz@gmail.com>
2025-02-09 15:30:15 +0200

    Add table information

--- cinema_frontend/src/components/AdminOptionComponents/ListOfFilms/ListOfFilms.jsx
+++ cinema_frontend/src/components/AdminOptionComponents/ListOfFilms/ListOfFilms.jsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button';
 import React, { useState } from 'react';
 
 const ListOfFilms = () => {
@@ -36,14 +37,31 @@ const ListOfFilms = () => {
         <div className='flex flex-col'>
             <p className="text-2xl font-bold mb-4">All film list</p>
             <div className="border rounded-lg overflow-hidden">
-                <div className='grid grid-cols-5 bg-gray-100 font-bold px-4 py-2'>
+                <div className='grid grid-cols-6 bg-gray-100 font-bold px-4 py-2'>
                     <div>ID</div>
                     <div>Film name</div>
                     <div>Film description</div>
                     <div>Release year</div>
                     <div>Genres</div>
+                    <div>Actions</div>
                 </div>
-                
+                {films.map((film) => (
+                    <div key={film.id} className="grid grid-cols-6 px-4 py-2 border-t">
+                        <div>{film.id}</div>
+                        <div>{film.name}</div>
+                        <div>{film.description}</div>
+                        <div>{film.release_year}</div>
+                        <div className="grid grid-cols-1 gap-4">
+                            {film.genres?.map((genre, index) => (
+                                <div key={index} className="w-20 px-3 py-1 text-[12px] shadow-lg rounded-lg bg-white">{genre}</div>
+                            ))}
+                        </div>
+                        <div className="flex gap-2">
+                            <Button variant="outline">Edit</Button>
+                            <Button variant="destructive">Delete</Button>
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
     )
