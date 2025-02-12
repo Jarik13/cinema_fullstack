50c6175fa1a18bea4f85cb0a5605c050803e1fef
Yaroslav Guz <aroslavguz@gmail.com>
2025-02-12 14:50:26 +0200

    Connect list of users from backend

--- cinema_frontend/src/components/AdminOptionComponents/ListOfUsers/ListOfUsers.jsx
+++ cinema_frontend/src/components/AdminOptionComponents/ListOfUsers/ListOfUsers.jsx
@@ -1,13 +1,15 @@
 import { Button } from '@/components/ui/button';
 import React, { useState } from 'react';
 import BlockUserCard from './BlockUserCard/BlockUserCard';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getUserList } from '@/redux/User/Action';
+import { store } from '@/redux/Store';
 
 const ListOfUsers = () => {
-    const dispatch = useDispatch();
-    const users = dispatch(getUserList());
+    const users = useSelector(store => store.userReducer.users);
     const [selectedUser, setSelectedUser] = useState(null);
+
+    console.log(users);
 
     const handleBlockClick = (user) => {
         setSelectedUser(user);
@@ -22,7 +24,7 @@ const ListOfUsers = () => {
         <div className='flex flex-col'>
             <p className="text-2xl font-bold mb-4">All Users List</p>
             <div className="border rounded-lg overflow-hidden">
-                <div className='grid grid-cols-[1fr_1fr_2fr_1fr_0.5fr] bg-gray-100 font-bold px-4 py-2'>
+                <div className='grid grid-cols-[2fr_1fr_2fr_1fr_0.5fr] bg-gray-100 font-bold px-4 py-2'>
                     <div>ID</div>
                     <div>Name</div>
                     <div>Email</div>
@@ -30,11 +32,11 @@ const ListOfUsers = () => {
                     <div>Action</div>
                 </div>
                 {users.map(user => (
-                    <div key={user.id} className="grid grid-cols-[1fr_1fr_2fr_1fr_0.5fr] border-t px-4 py-2 items-center">
-                        <div>{user.id}</div>
-                        <div>{user.name}</div>
-                        <div>{user.email}</div>
-                        <div>{user.age}</div>
+                    <div key={user.Id} className="grid grid-cols-[2fr_1fr_2fr_1fr_0.5fr] border-t px-4 py-2 items-center">
+                        <div>{user.Id}</div>
+                        <div>{user.UserName}</div>
+                        <div>{user.Email}</div>
+                        <div>{user.Age}</div>
                         <Button variant="destructive" onClick={() => handleBlockClick(user)}>Block</Button>
                     </div>
                 ))}
