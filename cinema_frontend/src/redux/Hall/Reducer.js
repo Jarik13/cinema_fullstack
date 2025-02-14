cbec9d6ca67b1dc19a37104a04eb1de2f4cf44d7
Yaroslav Guz <aroslavguz@gmail.com>
2025-02-14 21:12:49 +0200

    Add create hall action types

--- cinema_frontend/src/redux/Hall/Reducer.js
+++ cinema_frontend/src/redux/Hall/Reducer.js
@@ -1,3 +1,4 @@
+import { CREATE_HALL_FAILURE, CREATE_HALL_REQUEST, CREATE_HALL_SUCCESS } from "./ActionType";
 
 const initialState = {
     hall: null,
@@ -8,7 +9,12 @@ const initialState = {
 
 export const hallReducer = (state = initialState, action) => {
     switch (action.type) {
-        
+        case CREATE_HALL_REQUEST:
+            return { ...state, loading: true, error: null };
+        case CREATE_HALL_SUCCESS:
+            return { ...state, loading: false, hall: action.payload };
+        case CREATE_HALL_FAILURE:
+            return { ...state, loading: false, error: "Failed to create hall!" };
         default:
             return state;
     }
