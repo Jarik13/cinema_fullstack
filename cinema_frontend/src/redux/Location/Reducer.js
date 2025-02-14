10eaa8aea230469689caef5b40fa4302ea25e51b
Yaroslav Guz <aroslavguz@gmail.com>
2025-02-14 20:42:47 +0200

    Init location action types and reducer

---
+++ cinema_frontend/src/redux/Location/Reducer.js
@@ -0,0 +1,13 @@
+
+const initialState = {
+    hall: null,
+    halls: [],
+    loading: false,
+    error: null,
+};
+
+export const locationReducer = (state = initialState, action) => {
+    switch (action.type) {
+        
+    }
+}
--- cinema_frontend/src/redux/Store.js
+++ cinema_frontend/src/redux/Store.js
@@ -5,12 +5,14 @@ import { userReducer } from "./User/Reducer";
 import { filmReducer } from "./Film/Reducer";
 import { ticketReducer } from "./Ticket/Reducer";
 import { hallReducer } from "./Hall/Reducer";
+import { locationReducer } from "./Location/Reducer";
 
 const rootReducer = combineReducers({
     auth: authReducer,
     userReducer: userReducer,
     film: filmReducer,
     ticket: ticketReducer,
-    hall: hallReducer
+    hall: hallReducer,
+    location: locationReducer,
 });
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk))
