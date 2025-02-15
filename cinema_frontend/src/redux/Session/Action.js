34de4cd889b98d3c5b78b109ed790133633ff728
Yaroslav Guz <aroslavguz@gmail.com>
2025-02-15 10:47:52 +0200

    Create createSession function

---
+++ cinema_frontend/src/redux/Session/Action.js
@@ -0,0 +1,24 @@
+import { toast } from "react-toastify";
+import { CREATE_SESSION_FAILURE, CREATE_SESSION_REQUEST, CREATE_SESSION_SUCCESS } from "./ActionType"
+import axios from "axios";
+import { baseURL } from "@/config/constants";
+
+
+export const createSession = (data) => async (dispatch) => {
+    dispatch({ type: CREATE_SESSION_REQUEST });
+
+    try {
+        const response = await axios.post(`${baseURL}/api/Admin/CreateSession`, data, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`
+            }
+        });
+
+        console.log("Response", response);
+        dispatch({ type: CREATE_SESSION_SUCCESS });
+    } catch (e) {
+        console.log(e);
+        dispatch({ type: CREATE_SESSION_FAILURE });
+        toast.error("Failed to create session!");
+    }
+}
