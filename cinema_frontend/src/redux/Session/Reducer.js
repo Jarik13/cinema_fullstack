f2a2e104112bc16739568c37bc772ff7e8d4ddee
Yaroslav Guz <aroslavguz@gmail.com>
2025-02-15 13:07:00 +0200

    Update session reducer

--- cinema_frontend/src/redux/Session/Reducer.js
+++ cinema_frontend/src/redux/Session/Reducer.js
@@ -1,4 +1,4 @@
-import { CREATE_SESSION_FAILURE, CREATE_SESSION_REQUEST, CREATE_SESSION_SUCCESS } from "./ActionType";
+import { CREATE_SESSION_FAILURE, CREATE_SESSION_REQUEST, CREATE_SESSION_SUCCESS, GET_SESSION_LIST_FAILURE, GET_SESSION_LIST_REQUEST, GET_SESSION_LIST_SUCCESS } from "./ActionType";
 
 const initialState = {
     sessions: [],
@@ -9,10 +9,15 @@ const initialState = {
 export const sessionReducer = (state = initialState, action) => {
     switch (action.type) {
         case CREATE_SESSION_REQUEST:
+        case GET_SESSION_LIST_REQUEST:
             return { ...state, loading: true, error: null };
         case CREATE_SESSION_SUCCESS:
+            return { ...state, loading: false };
+        case GET_SESSION_LIST_SUCCESS:
             return { ...state, loading: false, sessions: action.payload };
         case CREATE_SESSION_FAILURE:
+            return { ...state, loading: false, error: "Faild to create sessions!" };
+        case GET_SESSION_LIST_FAILURE:
             return { ...state, loading: false, error: "Faild to get sessions!" };
         default:
             return state;
