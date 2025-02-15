a09cb437b26bf7c750d76449ac5340211d75c08d
Yaroslav Guz <aroslavguz@gmail.com>
2025-02-15 13:10:50 +0200

    Create getSessionList function

--- cinema_frontend/src/redux/Session/Action.js
+++ cinema_frontend/src/redux/Session/Action.js
@@ -1,5 +1,5 @@
 import { toast } from "react-toastify";
-import { CREATE_SESSION_FAILURE, CREATE_SESSION_REQUEST, CREATE_SESSION_SUCCESS } from "./ActionType"
+import { CREATE_SESSION_FAILURE, CREATE_SESSION_REQUEST, CREATE_SESSION_SUCCESS, GET_SESSION_LIST_FAILURE, GET_SESSION_LIST_REQUEST, GET_SESSION_LIST_SUCCESS } from "./ActionType"
 import axios from "axios";
 import { baseURL } from "@/config/constants";
 
@@ -21,3 +21,20 @@ export const createSession = (data) => async (dispatch) => {
     }
 }
 
+export const getSessionList = (showToast) => async (dispatch) => {
+    dispatch({ type: GET_SESSION_LIST_REQUEST });
+
+    try {
+        const { data } = await axios.get(`${baseURL}/api/Admin/ReadSessions`, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`
+            }
+        });
+        dispatch({ type: GET_SESSION_LIST_SUCCESS, payload: data });
+        toast.success("Session list getted successfully!");
+    } catch (e) {
+        console.log(e);
+        dispatch({ type: GET_SESSION_LIST_FAILURE });
+        toast.error("Failed to get session list!");
+    }
+}
