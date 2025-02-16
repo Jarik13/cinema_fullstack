e24e2f3355e042b2497b4d4ffa5050043edbc58c
Yaroslav Guz <aroslavguz@gmail.com>
2025-02-16 14:47:39 +0200

    Add deleteSnack action type to reducer

--- cinema_frontend/src/redux/Snack/Reducer.js
+++ cinema_frontend/src/redux/Snack/Reducer.js
@@ -1,4 +1,4 @@
-import { CREATE_SNACK_FAILURE, CREATE_SNACK_REQUEST, CREATE_SNACK_SUCCESS, GET_SNACK_LIST_FAILURE, GET_SNACK_LIST_REQUEST, GET_SNACK_LIST_SUCCESS } from "./ActionType";
+import { CREATE_SNACK_FAILURE, CREATE_SNACK_REQUEST, CREATE_SNACK_SUCCESS, DELETE_SNACK_FAILURE, DELETE_SNACK_REQUEST, DELETE_SNACK_SUCCESS, GET_SNACK_LIST_FAILURE, GET_SNACK_LIST_REQUEST, GET_SNACK_LIST_SUCCESS } from "./ActionType";
 
 const initialState = {
     snack: null,
@@ -11,15 +11,20 @@ export const snackReducer = (state = initialState, action) => {
     switch (action.type) {
         case CREATE_SNACK_REQUEST:
         case GET_SNACK_LIST_REQUEST:
+        case DELETE_SNACK_REQUEST:
             return { ...state, loading: true, error: null };
         case CREATE_SNACK_SUCCESS:
             return { ...state, loading: false, snack: action.payload };
         case GET_SNACK_LIST_SUCCESS:
             return { ...state, loading: false, snacks: action.payload };
+        case DELETE_SNACK_SUCCESS:
+            return { ...state, loading: false };
         case CREATE_SNACK_FAILURE:
             return { ...state, loading: false, error: "Failed to create snack!" };
         case GET_SNACK_LIST_FAILURE:
             return { ...state, loading: false, error: "Failed to get snack list!" };
+        case DELETE_SNACK_FAILURE:
+            return { ...state, loading: false, error: "Failed to delete snack!" };
         default:
             return state;
     }
