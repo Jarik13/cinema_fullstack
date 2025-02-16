6c8ed571f1f83e0a2f80206f0e38f7f91dbc7a8d
Yaroslav Guz <aroslavguz@gmail.com>
2025-02-16 14:33:17 +0200

    Update get snack list action type and add it to snack reducer

--- cinema_frontend/src/redux/Snack/Reducer.js
+++ cinema_frontend/src/redux/Snack/Reducer.js
@@ -1,4 +1,4 @@
-import { CREATE_SNACK_FAILURE, CREATE_SNACK_REQUEST, CREATE_SNACK_SUCCESS } from "./ActionType";
+import { CREATE_SNACK_FAILURE, CREATE_SNACK_REQUEST, CREATE_SNACK_SUCCESS, GET_SNACK_LIST_FAILURE, GET_SNACK_LIST_REQUEST, GET_SNACK_LIST_SUCCESS } from "./ActionType";
 
 const initialState = {
     snack: null,
@@ -10,11 +10,16 @@ const initialState = {
 export const snackReducer = (state = initialState, action) => {
     switch (action.type) {
         case CREATE_SNACK_REQUEST:
+        case GET_SNACK_LIST_REQUEST:
             return { ...state, loading: true, error: null };
         case CREATE_SNACK_SUCCESS:
             return { ...state, loading: false, snack: action.payload };
+        case GET_SNACK_LIST_SUCCESS:
+            return { ...state, loading: false, snacks: action.payload };
         case CREATE_SNACK_FAILURE:
             return { ...state, loading: false, error: "Failed to create snack!" };
+        case GET_SNACK_LIST_FAILURE:
+            return { ...state, loading: false, error: "Failed to get snack list!" };
         default:
             return state;
     }
