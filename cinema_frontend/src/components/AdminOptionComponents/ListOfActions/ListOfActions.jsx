2bf22b070d4c6c51f2876e043552e7d131c3a96d
Yaroslav Guz <aroslavguz@gmail.com>
2025-02-19 12:19:48 +0200

    Update action list table

--- cinema_frontend/src/components/AdminOptionComponents/ListOfActions/ListOfActions.jsx
+++ cinema_frontend/src/components/AdminOptionComponents/ListOfActions/ListOfActions.jsx
@@ -5,10 +5,10 @@ import DeleteActionCard from './DeleteActionCard/DeleteActionCard';
 
 const ListOfActions = () => {
     const [actions, setActions] = useState([
-        { id: 1, discount: 20, description: 'description 1', type: "Snack", is_active: true },
-        { id: 2, discount: 15, description: 'description 2', type: "Ticket", is_active: false },
-        { id: 3, discount: 10, description: 'description 3', type: "Ticket", is_active: true },
-        { id: 4, discount: 30, description: 'description 4', type: "Snack", is_active: false },
+        { id: 1, discount: 20, description: 'description 1', for_what: "Snack", discount_type: "Student", is_active: true },
+        { id: 2, discount: 15, description: 'description 2', for_what: "Ticket", discount_type: "BulkPurchase", is_active: false },
+        { id: 3, discount: 10, description: 'description 3', for_what: "Snack", discount_type: "ShowDateDiscount", is_active: true },
+        { id: 4, discount: 30, description: 'description 4', for_what: "Both", discount_type: "SummerForKids", is_active: false },
     ]);
 
     const [editingAction, setEditingAction] = useState(null);
@@ -36,20 +36,22 @@ const ListOfActions = () => {
         <div className='flex flex-col'>
             <p className="text-2xl font-bold mb-4">All Action List</p>
             <div className="border rounded-lg overflow-hidden">
-                <div className='grid grid-cols-6 bg-gray-100 font-bold px-4 py-2'>
+                <div className='grid grid-cols-7 bg-gray-100 font-bold px-4 py-2'>
                     <div>ID</div>
                     <div>Discount</div>
                     <div>Description</div>
-                    <div>Type</div>
+                    <div>Reason</div>
+                    <div>Discount Type</div>
                     <div>Active</div>
                     <div>Action</div>
                 </div>
                 {actions.map((action) => (
-                    <div key={action.id} className="grid grid-cols-6 px-4 py-2 border-b items-center">
+                    <div key={action.id} className="grid grid-cols-7 px-4 py-2 border-b items-center">
                         <div>{action.id}</div>
                         <div>{action.discount}%</div>
                         <div>{action.description}</div>
-                        <div>{action.type}</div>
+                        <div>{action.for_what}</div>
+                        <div>{action.discount_type}</div>
                         <div className={action.is_active ? "text-green-600" : "text-red-600"}>
                             {action.is_active ? "Active" : "Non Active"}
                         </div>
